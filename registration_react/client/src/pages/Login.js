e65b54da6b75f614391ad6e2ea3958d258d8c33d
Simon Ynzon <ynzonsimon@gmail.com>
2023-03-20 16:06:58 +0800

    Set up for React app

---
+++ registration_react/client/src/pages/Login.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import axios from 'axios'
+
+const Login = () => {
+    return (
+        <div>
+            <h1>This is the login page</h1>
+        </div>
+    )
+}
+
+export default Login
